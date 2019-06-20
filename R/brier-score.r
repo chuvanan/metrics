c17e544f120d98fdb41c5618b247b4bbf58f03ea
An Chu <chuvanan.cva@gmail.com>
2019-06-20 09:17:09 +0700

    Remove argument `cutoff` from mtr_brier_score()

--- R/brier-score.r
+++ R/brier-score.r
@@ -1,14 +1,14 @@
 
 
+
 ##' @export
-mtr_brier_score <- function(actual, predicted, cutoff = 0.5) {
+mtr_brier_score <- function(actual, predicted) {
 
     check_equal_length(actual, predicted)
     check_binary(actual)
-    check_cutoff_range(cutoff)
 
-    N <- length(predicted)
-    brier_score <- sum((predicted - actual)^2) / N
+    n <- length(predicted)
+    brier_score <- sum((actual - predicted)^2) / n
 
     brier_score
 }
