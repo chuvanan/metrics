7ec3f3835e840b2fc34d8dfc0eccfebf27327b5c
An Chu <chuvanan.cva@gmail.com>
2019-06-28 12:20:21 +0700

    Update error message in helper functions

--- R/helper-functions.r
+++ R/helper-functions.r
@@ -4,7 +4,7 @@
 check_equal_length <- function(actual, predicted) {
 
     if (length(actual) != length(predicted)) {
-        stop("`actual` and `predicted` must have equal length", call. = FALSE)
+        stop("`actual` and `predicted` must have equal length.", call. = FALSE)
     }
 
     invisible()
@@ -13,7 +13,7 @@ check_equal_length <- function(actual, predicted) {
 check_cutoff_range <- function(cutoff) {
 
     if (cutoff > 1 || cutoff < 0) {
-        stop("`cutoff` values must be in range of [0,1]", call. = FALSE)
+        stop("`cutoff` values must be in range of [0,1].", call. = FALSE)
     }
 
     invisible()
@@ -22,7 +22,7 @@ check_cutoff_range <- function(cutoff) {
 check_binary <- function(actual) {
 
     if (!all(actual %in% c(0, 1))) {
-        stop("`actual` only supports binay values: 0 & 1", call. = FALSE)
+        stop("`actual` only supports binary values: 0 & 1.", call. = FALSE)
     }
 
     invisible()
