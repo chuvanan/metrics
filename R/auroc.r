340145dd71ad596205d1ec646c90cfd3a6923546
An Chu <chuvanan.cva@gmail.com>
2019-07-16 22:34:02 +0700

    Implement AUROC using Mann-Whitney U test

--- R/auroc.r
+++ R/auroc.r
@@ -1,43 +1,66 @@
 
 
+
+##' @title
+##' Area Under ROC
+##'
+##' @description
+##'
+##' \code{mtr_auc_roc}
+##'
+##' @inheritParams classification_params
+##' @return A numeric scalar output
+##' @seealso \code{\link{mtr_ar}} \code{\link{mtr_auc_prc}}
+##' @author An Chu
+##' @examples
+##'
+##' set.seed(101)
+##' pred <- runif(1000)
+##' act <- round(pred)
+##' pred[sample(1000, 500)] <- runif(500)   # noise
+##' mtr_auc_prc(act, pred)
+##'
+##'
 ##' @export
 mtr_auc_roc <- function(actual, predicted) {
 
     check_equal_length(actual, predicted)
+    check_binary(actual)
 
-    r <- rank(predicted)
-    n_pos <- sum(actual == 1)
-    n_neg <- length(actual) - n_pos
-    sum_r <- sum(r[actual == 1])
+    ## This implementation uses Mann–Whitney U test for speed
+    ## r <- rank(predicted)            ##
+    ## n_pos <- sum(actual == 1)       ##
+    ## n_neg <- length(actual) - n_pos ##
+    ## sum_r <- sum(r[actual == 1])    ##
+    ## auc <- (sum_r - n_pos * (n_pos + 1) / 2) / (n_pos * n_neg) ##
 
-    auc <- (sum_r - n_pos * (n_pos + 1) / 2) / (n_pos * n_neg)
+    auc <- auc_(actual, predicted)
     auc
 }
 
 
-##' @title 
+##' @title
 ##' Accuracy Ratio
-##' 
-##' @description 
-##' 
+##'
+##' @description
+##'
 ##' \code{mtr_ar}
-##' 
+##'
 ##' @inheritParams classification_params
 ##' @return A numeric scalar output
 ##' @author An Chu
 ##' @name accuracy_ratio
-##' @examples 
-##' 
+##' @examples
+##'
 ##' set.seed(101)
 ##' pred <- runif(1000)
 ##' act <- round(pred)
 ##' pred[sample(1000, 500)] <- runif(500)   # noise
 ##' mtr_accuracy_ratio(act, pred)
-##' 
-##' 
+##'
+##'
 ##' @export
 mtr_ar <- function(actual, predicted) {
-    auc <- mtr_auc(actual, predicted)
     auc <- mtr_auc_roc(actual, predicted)
     ar <- 2 * auc - 1
     ar
